b7fd76d3dbd647aa4615adba108efe8ac74cf18a
Shanti <64201601+shanti-sudo@users.noreply.github.com>
2024-06-21 13:19:15 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = new Member();
+    member.skills = [
+        "PHP",
+        "Java",
+        "JavaScript",
+        "NodeJS",
+        "Angular",
+        "React",
+        "Vue"
+    ];
+    return member;
+}
